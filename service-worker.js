fcf49bd7c55054468e522ed9e51002215c0700bf
Justin Alexia Andrew <103948035+Justin-io@users.noreply.github.com>
2024-10-31 16:10:33 +0530

    chehk 3

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,44 @@
-// service-worker.js
+const CACHE_NAME = "v1";
+const ASSETS_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/assets/css/main.css",
+    "/assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+    "/assets/js/main.js",
+    "/assets/vendor/aos/aos.js",
+    "/assets/images/icon-192x192.png",
+    "/assets/images/icon-512x512.png"
+];
+
+// Install Service Worker and cache assets
 self.addEventListener("install", (event) => {
     event.waitUntil(
-      caches.open("static-cache").then((cache) => {
-        return cache.addAll([
-          "/", // Cache the root
-          "/index.html", // Cache the main HTML
-          "/css/style.css", // Cache CSS file
-          "/js/script.js", // Cache JS file
-          "/path/to/icon-192x192.png", // Cache icons
-          "/path/to/icon-512x512.png"
-        ]);
-      })
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
     );
-  });
-  
-  self.addEventListener("fetch", (event) => {
+});
+
+// Activate Service Worker and clean old caches
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event to serve cached assets
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
-  });
-  
+});
